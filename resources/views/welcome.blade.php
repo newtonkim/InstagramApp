26754ed15921a134f76368be70246a1014d28ef6
newtonkim <knewts98@gmail.com>
2021-09-09 16:06:07 +0300

    Add username, bootstrap layout sass

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    InstagramApp
+                    InstaApp Welcome
                 </div>
 
                 <div class="links">
