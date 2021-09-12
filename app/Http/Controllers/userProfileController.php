cb349ea3934a13d843a2abd1f56b560defff5e5e
newtonkim <knewts98@gmail.com>
2021-09-13 00:03:32 +0300

    Introduce Cache Concept in the userprofilecontroller

--- app/Http/Controllers/userProfileController.php
+++ app/Http/Controllers/userProfileController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cache;
 use Intervention\Image\Facades\Image;
 
 class userProfileController extends Controller
@@ -12,9 +13,28 @@ class userProfileController extends Controller
     {
         $follows = (auth()->user()) ? auth()->user()->following->contains($user->id) : false;
         
-        $postCount = $user->posts->count();
-        $followersCount = $user->profile->followers->count();
-        $followingCount = $user->following->count();
+        /* Introducing the Cache concept for faster loading
+        */
+        $postCount = Cache::remember(
+            'count.posts.' . $user->id,
+            now()->addSeconds(30),
+            function () use ($user) {
+                return $user->posts->count();
+            });
+
+        $followersCount = Cache::remember(
+            'count.followers.' . $user->id,
+            now()->addSeconds(30),
+            function() use ($user) {
+                return $user->profile->followers->count();
+            });
+
+        $followingCount = Cache::remember(
+            'count.following.' . $user->id,
+            now()->addSeconds(30),
+            function() use ($user){
+                return $user->following->count();
+            });
         
         return view('profiles.index', compact(
             'user', 
